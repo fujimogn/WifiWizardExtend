78daa5c9e10adf560798653c6eee135850bf972a
fujimogn <fujimogn@gmail.com>
2016-06-20 18:09:35 +0900

    fix exports

--- www/WifiWizard.js
+++ www/WifiWizard.js
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -248,7 +248,7 @@ var WifiWizard = {
         }
         cordova.exec(win, fail, 'WifiWizard', 'getConnectedSSID', []);
     },
-    
+
     getCurrentNetwork: function(win, fail) {
         if (typeof win != "function") {
             console.log("getCurrentNetwork first parameter must be a function to handle network.");
@@ -256,8 +256,8 @@ var WifiWizard = {
         }
         cordova.exec(win, fail, 'WifiWizard', 'getConnectedNetwork', []);
     },
-    
-    
+
+
     /**
      *  Gets 'true' or 'false' if WiFi is enabled or disabled
      * @param 	win	callback function
@@ -293,4 +293,6 @@ var WifiWizard = {
     }
 };
 
-module.exports = WifiWizard;
+if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.exports === exports){
+  module.exports = WifiWizard;
+}
